b2482fc4b0353e3202d66a03478f1d325dd9d118
lize <lz632044343@163.com>
2022-08-23 16:57:53 +0800

    改端口

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,19 +5,32 @@ import com.example.demo.service.UserService;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.GsonBuilderUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
     @Autowired
     UserService service;
 
-        @RequestMapping(value = "/getUserItem", method = RequestMethod.GET)
-    public String getUserItem() {
+    @RequestMapping(value = "/getUserItem", method = RequestMethod.GET)
+    public String getUserItem(Integer id) {
         User user = service.getUserInfo();
+        System.out.println("" + id);
         return new Gson().toJson(user);
     }
 
+    @RequestMapping(value = "/getTest", method = RequestMethod.POST)
+    public String getTest(Integer id) {
+        User user = service.getUserInfo();
+        System.out.println("" + id);
+        return new Gson().toJson(user);
+    }
+
+    @RequestMapping(value = "/getTest2", method = RequestMethod.GET)
+    public String getTest2(Integer id, @RequestParam(required = false) @RequestHeader("token") String token) {
+        User user = service.getUserInfo();
+        System.out.println("" + id);
+        System.out.println("" + token);
+        return new Gson().toJson(user);
+    }
 }
